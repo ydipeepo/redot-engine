ad8b971a55f97b3a1e6e664b45998fcc18925ad3
Spartan322 <Megacake1234@gmail.com>
2024-10-27 10:31:11 -0400

    Fix copyright_headers.py referring to Godot Fix create_test.py missing Redot copyright reference
    (cherry picked from commit 08b664511b0b984309dab13efc27ad2703d4ff18)

--- tests/create_test.py
+++ tests/create_test.py
@@ -49,9 +49,11 @@ def main():
 /*  test_{name_snake_case}.h {padding} */
 /**************************************************************************/
 /*                         This file is part of:                          */
-/*                             GODOT ENGINE                               */
-/*                        https://godotengine.org                         */
+/*                             REDOT ENGINE                               */
+/*                        https://redotengine.org                         */
 /**************************************************************************/
+/* Copyright (c) 2024-present Redot Engine contributors                   */
+/*                                          (see REDOT_AUTHORS.md)        */
 /* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
 /* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
 /*                                                                        */
